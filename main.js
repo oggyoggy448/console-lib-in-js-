425fc4ad0ee8fdd1d7d51151f3b4b3316ad6418e
OggyOggy448 <sign448free@gmail.com>
2019-08-08 16:43:05 +0800

    added

---
+++ main.js
@@ -0,0 +1,35 @@
+// log messages
+// console.log('log message');
+
+
+// error message 
+// console.error('error message');
+
+// warning message
+// console.warn('warning');
+
+// show a result of object in form of table
+// console.table({'n':'Google',        type: 'browser'});
+
+// clear the console window 
+// console.clear();
+
+// show the object all properties
+// console.dir(document);
+
+// show the message in from of group
+// console.group('group msg');
+// console.log('msg');
+// console.error('error');
+// console.warn('warning');
+// console.groupEnd('closing');
+
+// show the time of execution
+// console.time('time');
+    // any code written in b/w would show the total time of executation 
+// console.timeEnd('closed');
+
+
+// used for testing a specific code
+// console.assert(2>3,"msg has been passed");
+
